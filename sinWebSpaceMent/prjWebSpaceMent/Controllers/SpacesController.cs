eed1a80173cefc782a05bdfcd62b80c64520a43b
Tsai-Chi-Ling <aodklbj@gmail.com>
2022-02-10 12:04:38 +0800

    G0_場地清單及刪除
    0210 基本場地清單及刪除

---
+++ sinWebSpaceMent/prjWebSpaceMent/Controllers/SpacesController.cs
@@ -0,0 +1,30 @@
+﻿using prjWebSpaceMent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjWebSpaceMent.Controllers
+{
+    public class SpacesController : Controller
+    {
+        // GET: Spaces
+        public ActionResult Spaces_List()
+        {
+            // 場地總覽
+            var datas = from p in (new SPACEMENTEntities()).Spaces
+                        select p;
+            return View(datas);
+        }
+
+        public ActionResult Spaces_Delete(int? id)
+        {
+            if (id != null)
+            {
+                (new CSpacesFactory()).delete((int)id);
+            }
+            return RedirectToAction("Spaces_List");
+        }
+    }
+}
