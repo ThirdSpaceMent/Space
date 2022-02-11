fe5db42292749e625b9b84fb0422d0f7d5cb3911
Karsten Hong <95757969+KarstenHong@users.noreply.github.com>
2022-02-12 02:40:27 +0800

    豪祥
    加上龍哥部分(更新中....)

---
+++ sinWebSpaceMent/prjWebSpaceMent/Controllers/MemberController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using prjWebSpaceMent.Models;
+
+namespace prjWebSpaceMent.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        dbSpaceMentEntities1 db = new dbSpaceMentEntities1();
+        // GET: Member
+        public ActionResult Index()
+        {
+            var space =
+                db.Spaces.OrderByDescending(m => m.sNumber).ToList();
+            return View("../Home/Index", "_LayoutMember", space);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
+        public ActionResult ShoppingCar()
+        {
+            string oAccount = User.Identity.Name;
+            var OrderDetails = db.Orders.Where
+                (m => m.oAccount == oAccount)
+                .ToList();
+            return View(OrderDetails);
+        }
+    }
+}
