ceb9982bd3be3cc9b49c237d92d1445c616eee9b
Tsai-Chi-Ling <aodklbj@gmail.com>
2022-02-11 15:56:39 +0800

    G0_基本新增功能
    1.基本場地新增功能
    (基本驗證required)
    
    2.增加Spaces_Create頁面
    (目前跳轉回LIST頁面)
    
    3.類別增加CSpaces

---
+++ sinWebSpaceMent/prjWebSpaceMent/Models/CSpaces.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace prjWebSpaceMent.Models
+{
+    public class CSpaces
+    {
+
+        public int sNumber { get; set; }
+
+        public string sName { get; set; }
+
+        
+        public string sType { get; set; }
+
+        
+        public string sAddr { get; set; }
+
+        
+        public string sPhone { get; set; }
+
+        
+        public string sFloor { get; set; }
+
+        public string sHeight { get; set; }
+
+        public string sArea { get; set; }
+
+       
+        public string sCapacity { get; set; }
+
+        
+        public decimal sRent { get; set; }
+
+        
+        public decimal sRate { get; set; }
+
+       
+        public string sIntro { get; set; }
+
+        
+        public string sOpeningTime { get; set; }
+
+        public string sSecurity { get; set; }
+
+        public string sTraffic { get; set; }
+
+        //public System.DateTime sCreated_at { get; set; }
+
+        //public System.DateTime sUpdated_at { get; set; }
+
+        //public Nullable<int> FK_Space_to_Owner { get; set; }
+    }
+}
--- sinWebSpaceMent/prjWebSpaceMent/Spaces.cs
+++ sinWebSpaceMent/prjWebSpaceMent/Spaces.cs
@@ -11,7 +11,9 @@ namespace prjWebSpaceMent
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Spaces
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -25,23 +27,59 @@ namespace prjWebSpaceMent
             this.SubSpaces = new HashSet<SubSpaces>();
         }
     
+        
+        [DisplayName("序號")]
         public int sNumber { get; set; }
+
+        [DisplayName("場地名稱")]
         public string sName { get; set; }
+
+        [DisplayName("場地類型")]
         public string sType { get; set; }
+
+        [DisplayName("地址")]
         public string sAddr { get; set; }
+
+        [DisplayName("聯絡電話")]
         public string sPhone { get; set; }
+
+        [DisplayName("樓層")]
         public string sFloor { get; set; }
+
+        [DisplayName("高度")]
         public string sHeight { get; set; }
+
+        [DisplayName("面積")]
         public string sArea { get; set; }
+
+        [DisplayName("可容納人數")]
         public string sCapacity { get; set; }
+
+        [DisplayName("費用")]
         public decimal sRent { get; set; }
+
+        [DisplayName("費率")]
         public decimal sRate { get; set; }
+
+        [DisplayName("簡介")]
         public string sIntro { get; set; }
+
+        [DisplayName("營業時間")]
         public string sOpeningTime { get; set; }
+
+        [DisplayName("場地安全")]
         public string sSecurity { get; set; }
+
+        [DisplayName("交通特色")]
         public string sTraffic { get; set; }
+
+        [DisplayName("建立時間")]
         public System.DateTime sCreated_at { get; set; }
+
+        [DisplayName("更新時間")]
         public System.DateTime sUpdated_at { get; set; }
+
+        [DisplayName("會員編號")]
         public Nullable<int> FK_Space_to_Owner { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
